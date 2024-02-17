b8e250caf97d67137cffb7b15b1efc7ea2f975c3
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2024-02-17 18:16:11 +0100

    neighbor func

--- utilities/graph.py
+++ utilities/graph.py
@@ -1,4 +1,5 @@
 import numpy as np
+from stack import Stack
 
 class Graph:
     def __init__(self, num_vertices):
@@ -25,6 +26,18 @@ class Graph:
     def edge_numbers(self):
         return np.sum(self.adj_matrix)//2
     
+    def get_neigh(self, v):
+        v = ord(v.lower()) - ord('a')
+        return [chr(ord('a')+index) for index, node in enumerate(self.adj_matrix[v]) if node==1]
+
+    def bfs(self):
+        s = Stack()
+        visited = ['a']
+        s.push('a')
+        while not s.is_empty():
+            pass
+
+    
 if __name__ == "__main__":
     graph = Graph(5)
 
@@ -37,3 +50,4 @@ if __name__ == "__main__":
     print(graph.get_degree('a'))
     print(graph.get_degree('b'))
     print(graph.edge_numbers())
+    print(graph.get_neigh('A'))
